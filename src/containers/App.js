590391a409762cb82e9eca91129041d47748a6ea
Christiane Repunte <repuntechannix@gmail.com>
2021-10-11 18:29:06 +0800

    Spelling Check

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
-import ErrorBoundry from '../components/ErrorBoundry';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 import { robots } from "../robots"; //robots.js
 
@@ -41,9 +41,9 @@ class App extends Component{
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <ErrorBoundry>
+                        <ErrorBoundary>
                             <CardList robots={filteredRobots}/> 
-                        </ErrorBoundry>
+                        </ErrorBoundary>
                     </Scroll>
                 </div> 
             );
