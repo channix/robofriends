ab4f624b9771fba7a3ecf4096beb943dbb4c3e6e
Christiane Repunte <repuntechannix@gmail.com>
2021-10-11 10:38:45 +0800

    Robofriends uploading

---
+++ src/robots.js
@@ -0,0 +1,82 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Channix',
+        username: 'xhristiane',
+        email: 'repuntechannix@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Yann Zashro',
+        username: 'zashro',
+        email: 'ryanotaydemusa@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Dan Jerson',
+        username: 'dannyj',
+        email: 'danhermoso@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Jager Mael',
+        username: 'dwineyboi',
+        email: 'dwinenadal@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Jack Carry',
+        username: 'boljak',
+        email: 'jackmatawa@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Gabby Wafu',
+        username: 'gabgab',
+        email: 'gabrielsopsop@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Drake Jaycel',
+        username: 'drakeflacko',
+        email: 'iverpunayan@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Niel The Barn',
+        username: 'fredtabar',
+        email: 'nielfredtabar@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Zebedee Zyx',
+        username: 'idolbedee',
+        email: 'zebedeeescuzar@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Bro Cris',
+        username: 'cocris',
+        email: 'crisco@gmail.com'
+    },
+    {
+        id: 11,
+        name: 'Anne Jon',
+        username: 'annealipin',
+        email: 'annelabasan@gmail.com'
+    },
+    {
+        id: 12,
+        name: 'Boss Daws',
+        username: 'dawson',
+        email: 'dawsonbrycesanchez@gmail.com'
+    }
+
+
+
+
+
+
+
+
+]
